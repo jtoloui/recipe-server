a6ecdc34b5a73ee0af2c01467a74ce47821083b6
Jamie Toloui <jamie@toloui.com>
2023-06-05 21:24:47 +0100

    enable creds on cors

--- src/utils/cors.ts
+++ src/utils/cors.ts
@@ -1,7 +1,8 @@
-const reactAppOrigin = "https://localhost:3000";
+const reactAppOrigin = 'https://localhost:3000';
 
 export const corsOptions = {
-	origin: reactAppOrigin,
-	allowedHeaders: ["Content-Type", "Authorization"],
-	methods: ["GET", "POST", "PUT", "DELETE"],
+  origin: reactAppOrigin,
+  credentials: true,
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
 };
