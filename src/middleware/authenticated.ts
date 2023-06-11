2c67ef37775fc28cac664d78cd0ec43eeac2e40a
Jamie Toloui <jamie@toloui.com>
2023-06-11 19:34:55 +0100

    update is auth middleware to validate the token

--- src/middleware/authenticated.ts
+++ src/middleware/authenticated.ts
@@ -1,11 +1,17 @@
 import { CognitoIdentityProvider } from '@aws-sdk/client-cognito-identity-provider';
+import axios from 'axios';
 import { NextFunction, Request, Response } from 'express';
+import jwt, { JwtHeader } from 'jsonwebtoken';
+import jwkToPem, { RSA } from 'jwk-to-pem';
 
 import { poolData } from '../auth/awsCognito';
 import logger from '../logger/winston';
 
 const winstonLogger = logger('info', 'Authentication Middleware');
 
+interface MyJWK extends RSA {
+  kid: string;
+}
 export const isAuthenticated = async (
   req: Request,
   res: Response,
@@ -40,7 +46,76 @@ export const isAuthenticated = async (
         .status(401)
         .json({ message: 'Unauthorized: User is disabled' });
     }
-    next();
+
+    // Get JWT header
+    const { header } = jwt.decode(sessionToken, { complete: true }) as {
+      header: JwtHeader;
+    };
+
+    // Fetch the public JWKS
+    const {
+      data: { keys },
+    } = await axios.get<{ keys: MyJWK[] }>(
+      `https://cognito-idp.${process.env.AWS_REGION}.amazonaws.com/${poolData.UserPoolId}/.well-known/jwks.json`
+    );
+
+    // Find the public key that matches the JWT header
+    const publicKey = keys.find((key) => key.kid === header.kid);
+
+    if (!publicKey) {
+      winstonLogger.error(`[isAuthenticated]: Forbidden - Invalid token`);
+      return res.status(401).json({ message: 'Forbidden: Invalid token' });
+    }
+
+    // Convert the JWK to PEM format
+    const pem = jwkToPem(publicKey);
+
+    // Verify the JWT
+    jwt.verify(
+      sessionToken,
+      pem,
+      { algorithms: ['RS256'] },
+      (err, decodedToken) => {
+        if (err) {
+          winstonLogger.error(`[isAuthenticated]: Forbidden - Invalid token`);
+          return res.status(401).json({ message: 'Forbidden: Invalid token' });
+        }
+
+        if (typeof decodedToken !== 'object' || decodedToken === null) {
+          winstonLogger.error(
+            `[isAuthenticated]: Forbidden - Invalid token payload`
+          );
+          return res
+            .status(401)
+            .json({ message: 'Forbidden: Invalid token payload' });
+        }
+
+        // Validate the issuer
+        if (
+          decodedToken.iss !==
+          `https://cognito-idp.${process.env.AWS_REGION}.amazonaws.com/${poolData.UserPoolId}`
+        ) {
+          winstonLogger.error(
+            `[isAuthenticated]: Forbidden - Invalid token issuer`
+          );
+          return res
+            .status(401)
+            .json({ message: 'Forbidden: Invalid token issuer' });
+        }
+
+        // Validate the audience (app client ID)
+        if (decodedToken.aud !== poolData.ClientId) {
+          winstonLogger.error(
+            `[isAuthenticated]: Forbidden - Invalid token audience`
+          );
+          return res
+            .status(401)
+            .json({ message: 'Forbidden: Invalid token audience' });
+        }
+
+        next();
+      }
+    );
   } catch (error) {
     winstonLogger.error(
       `[isAdmin]: Unauthorized - [UserId]: ${req.session.user?.sub} - ${error}`
@@ -56,8 +131,6 @@ export const isAdmin = async (
 ) => {
   await isAuthenticated(req, res, async () => {
     try {
-      console.log('hello');
-
       const params = {
         UserPoolId: poolData.UserPoolId,
         Username: req.session?.user?.username,
