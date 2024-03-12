2cff947bd365c69e016f9adb1b781c544541bfd1
Jamie Toloui <jamie@toloui.com>
2024-03-12 21:38:24 +0000

    - refactor layers for config - refactor db and store connection - pass config as dependency injection

--- index.ts
+++ index.ts
@@ -1,8 +1,6 @@
 import bodyParser from 'body-parser';
-import MongoDBStore from 'connect-mongodb-session';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
-import dotenv from 'dotenv';
 import express, { Express } from 'express';
 import session from 'express-session';
 import expressWinston from 'express-winston';
@@ -10,49 +8,36 @@ import fs from 'fs';
 import helmet from 'helmet';
 import https from 'https';
 
-import { connectDB } from './src/db';
+import { DBConnection } from './src/db';
 import logger from './src/logger/winston';
 import assignId from './src/middleware/requestId';
 // routes
-import apiRoutes from './src/routes/apiRoutes';
+import { apiRoutes } from './src/routes/apiRoutes';
 import authRoutes from './src/routes/authRoutes';
 import { corsOptions } from './src/utils/cors';
+import { newConfig } from './src/config/config';
 
-dotenv.config();
-const MongoDBStores = MongoDBStore(session);
+const config = new newConfig(logger).validate().getConfig();
 
-connectDB();
+const dbConnection = new DBConnection(config);
+dbConnection.connectDB();
+dbConnection.connectSessionStore();
+const store = dbConnection.getSessionStore();
+
 const app: Express = express();
 const port = process.env.PORT;
 
 // allow proxy from cloudfare
 app.set('trust proxy', 1);
-// Setup MongoDB session store
-const mongoUri = process.env.MONGODB_URI || '';
 
-const store = new MongoDBStores({
-  uri: mongoUri,
-  databaseName: process.env.MONGODB_SESSION_DB || 'recipe-app',
-  collection: process.env.MONGODB_SESSION_COLLECTION || 'session',
-  expires: 1000 * 60 * 60 * 24 * 7, // 1 week
-});
-
-const domain = process.env.API_APP_URI || '';
+const domain = config.apiAppUri;
 const domainParts = domain.split('.');
 const domainRoot = domainParts.slice(1).join('.');
 const domainRootWithDot = `.${domainRoot}`;
 // winston logger
-const logLevel = process.env.LOG_LEVEL || 'info';
-const winstonLogger = logger(logLevel, 'server');
-const winstonLoggerMiddleware = logger(logLevel);
-
-store.on('error', (error) => {
-  winstonLogger.error(error);
-});
-
-store.on('connected', () => {
-  winstonLogger.info('MongoDB session store connected');
-});
+const logLevel = config.logLevel;
+const serverLogger = config.newLogger(logLevel, 'server');
+const winstonLoggerMiddleware = config.newLogger(logLevel, 'Routes');
 
 // middleware - custom
 app.use(assignId);
@@ -67,18 +52,18 @@ app.use(cookieParser());
 
 app.use(
   session({
-    secret: process.env.AUTH0_SECRET || '',
+    secret: config.sessionSecret || '',
     cookie: {
       maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
       secure: true,
       httpOnly: true,
-      domain: `.${process.env.COOKIE_DOMAIN}`,
+      domain: `.${config.cookieDomain}`,
       sameSite: 'lax',
     },
     store: store,
     resave: false,
     saveUninitialized: false,
-  })
+  }),
 );
 
 // Middleware to log HTTP requests
@@ -101,26 +86,26 @@ app.use(
       } - HTTP ${req.method} ${req.url} - Status: ${res.statusCode} - ${
         res.statusMessage
       }`,
-  })
+  }),
 );
 
 // Routes
 app.use('/auth', authRoutes);
-app.use('/api', apiRoutes);
+app.use('/api', apiRoutes(config));
 
 if (process.env.NODE_ENV !== 'production') {
   const key = fs.readFileSync('./certs/localhost-key.pem');
   const cert = fs.readFileSync('./certs/localhost.pem');
 
   https.createServer({ key, cert }, app).listen(port, () => {
-    winstonLogger.info(
-      `⚡️[server]: Server is running at https://localhost:${port}`
+    serverLogger.info(
+      `⚡️[server]: Server is running at https://localhost:${port}`,
     );
   });
 } else {
   app.listen(port, () => {
-    winstonLogger.info(
-      `⚡️[server]: Server is running at http://localhost:${port}`
+    serverLogger.info(
+      `⚡️[server]: Server is running at http://localhost:${port}`,
     );
   });
 }
